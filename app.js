b271721bfadc3567b02cac1f73c2e1f1d331251e
Saqlain Ali Shah <113901166+Saqlainalyshah@users.noreply.github.com>
2024-05-26 12:27:15 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
 console.log("Hello Jenkins!");
-console.log("Hello Jenkins By Saqlain!");
 
-const add = (a, b) =>{
-  return a+b
-}
 
-console.log("Sum of the value = ",add(5,5));
-console.log("Hello Nodejs from github.");
-console.log("Hello from Saqlain.");
+
 
